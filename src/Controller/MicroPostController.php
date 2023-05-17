3dc20020df153216029e0a56c7795b789c5d8519
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-05-17 22:41:14 +0200

    Repositories commit

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
+//        $microPost = new MicroPost();
+//        $microPost->setTitle('Post from controller');
+//        $microPost->setText('Post from controller');
+//        $microPost->setCreated(new DateTime());
+        $microPost = $posts->find(5);
+
+        $posts->remove($microPost, true);
+//        dd($posts->find(4));
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
