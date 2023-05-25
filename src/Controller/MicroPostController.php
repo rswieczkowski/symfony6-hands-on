26f9adb38ae5f6a55a3c59932d50c953e74d423b
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-05-25 14:27:30 +0200

    Getting posts commit

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -14,23 +14,16 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        dd($posts->findAll());
-//        $microPost = new MicroPost();
-//        $microPost->setTitle('Post from controller');
-//        $microPost->setText('Post from controller');
-//        $microPost->setCreated(new DateTime());
-        $microPost = $posts->find(5);
-
-        $posts->remove($microPost, true);
-//        dd($posts->find(4));
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
         ]);
     }
 
     #[Route('/micro-post/{post}', name: 'app_micropost_show')]
     public function showOne(MicroPost $post): Response
     {
-        dd($post);
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
     }
 }
